a20eed2e0a0c8607fae0a279705f516365b6b1de
Oliver Benz <oliver.benz@outlook.com>
2022-08-14 18:17:32 +0200

    Allow subdirectory in output folder for header files.

--- CustomCommands.cmake
+++ CustomCommands.cmake
@@ -1,12 +1,15 @@
 # ----- Helper Functions -----
-function(copy_headers_to_output targetName headerFiles)
+function(copy_headers_to_output targetName headerFiles subDirectory)
     foreach(filePath ${headerFiles})
         get_filename_component(fileName ${filePath} NAME)
+
+        string(TOLOWER ${CMAKE_PROJECT_NAME} lowerProjectName)
+        set(outputPath ${CMAKE_BINARY_DIR}/out/include/${lowerProjectName}/${subDirectory}/${fileName})
 
         add_custom_command(
             TARGET ${targetName}
             POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${filePath} "${CMAKE_BINARY_DIR}/out/include/${targetName}/${fileName}"
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${filePath} ${outputPath}
         )
     endforeach()
 endfunction()
