f27967808578ce4273ac7aede103e9c3a0ea2ac2
Oliver Benz <oliver.benz@outlook.com>
2023-02-24 19:28:20 +0100

    Add documentation in CustomCommands.

--- CustomCommands.cmake
+++ CustomCommands.cmake
@@ -1,4 +1,11 @@
 # ----- Helper Functions -----
+
+# Copy the header files of a target to the output directory in a postbuild script
+#
+# targetName   .. Name of the target responsible for copying the header files
+# headerFiles  .. List of all header files to copy to the output
+# subDirectory .. Subdirectory in 'out/include' where to copy the header files to
+#
 function(copy_headers_to_output targetName headerFiles subDirectory)
     foreach(filePath ${headerFiles})
         get_filename_component(fileName ${filePath} NAME)
