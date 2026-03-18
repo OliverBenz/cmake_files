b811f219f32ec3b16964072e83d38b7cd6604534
Oliver Benz <oliver.benz@outlook.com>
2026-03-18 07:06:12 +0100

    Configs: Fix multitarget werror warning.

--- Configurations/GlobalDefaults.cmake
+++ Configurations/GlobalDefaults.cmake
@@ -1,68 +1,64 @@
-# Sets the most useful compiler warning tags for different compilers for each specific target.
-
 # Use this function only on the predefined interface libraries.
 function(set_target_options_warnings targetName)
-	option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
-
 	set(MSVC_WARNINGS
-			/W4 # Baseline reasonable warnings
-			/w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
-			/w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
-			/w14263 # 'function': member function does not override any base class virtual member function
-			/w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
-			# be destructed correctly
-			/w14287 # 'operator': unsigned/negative constant mismatch
-			/we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
-			# the for-loop scope
-			/w14296 # 'operator': expression is always 'boolean_value'
-			/w14311 # 'variable': pointer truncation from 'type1' to 'type2'
-			/w14545 # expression before comma evaluates to a function which is missing an argument list
-			/w14546 # function call before comma missing argument list
-			/w14547 # 'operator': operator before comma has no effect; expected operator with side-effect
-			/w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?
-			/w14555 # expression has no effect; expected expression with side- effect
-			/w14619 # pragma warning: there is no warning number 'number'
-			/w14640 # Enable warning on thread un-safe static member initialization
-			/w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.
-			/w14905 # wide string literal cast to 'LPSTR'
-			/w14906 # string literal cast to 'LPWSTR'
-			/w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
-			/permissive- # standards conformance mode for MSVC compiler.
-			)
+		/W4 # Baseline reasonable warnings
+
+		/w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
+		/w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
+		/w14263 # 'function': member function does not override any base class virtual member function
+		/w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
+
+		# Be destructed correctly
+		/w14287 # 'operator': unsigned/negative constant mismatch
+		/we4289 # Nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
+
+		# The for-loop scope
+		/w14296 # 'operator': expression is always 'boolean_value'
+		/w14311 # 'variable': pointer truncation from 'type1' to 'type2'
+		/w14545 # Expression before comma evaluates to a function which is missing an argument list
+		/w14546 # Function call before comma missing argument list
+		/w14547 # 'operator': operator before comma has no effect; expected operator with side-effect
+		/w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?
+		/w14555 # Expression has no effect; expected expression with side- effect
+		/w14619 # Pragma warning: there is no warning number 'number'
+		/w14640 # Enable warning on thread un-safe static member initialization
+		/w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.
+		/w14905 # Wide string literal cast to 'LPSTR'
+		/w14906 # String literal cast to 'LPWSTR'
+		/w14928 # Illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+
+		$<$<CONFIG:Release,RelWithDebInfo,MinSizeRel>:/WX> # Warnings as errors
+	)
 
 	set(CLANG_WARNINGS
-			-Wall
-			-Wextra # reasonable and standard
-			-Wshadow # warn the user if a variable declaration shadows one from a parent context
-			-Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
-			# catch hard to track down memory errors
-			-Wold-style-cast # warn for c-style casts
-			-Wcast-align # warn for potential performance problem casts
-			-Wunused # warn on anything being unused
-			-Woverloaded-virtual # warn if you overload (not override) a virtual function
-			-Wpedantic # warn if non-standard C++ is used
-			-Wconversion # warn on type conversions that may lose data
-			-Wsign-conversion # warn on sign conversions
-			-Wnull-dereference # warn if a null dereference is detected
-			-Wdouble-promotion # warn if float is implicit promoted to double
-			-Wformat=2 # warn on security issues around functions that format output (ie printf)
-			)
+		-Wall
+		-Wextra                      # Reasonable and standard
+		-Wshadow                     # Warn the user if a variable declaration shadows one from a parent context
+		-Wnon-virtual-dtor           # Warn the user if a class with virtual functions has a non-virtual destructor. This helps
 
-	if(WARNINGS_AS_ERRORS)
-		if("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
-			set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
-			set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
-		endif()
-	endif()
+		# Catch hard to track down memory errors
+		-Wold-style-cast             # Warn for c-style casts
+		-Wcast-align                 # Warn for potential performance problem casts
+		-Wunused                     # Warn on anything being unused
+		-Woverloaded-virtual         # Warn if you overload (not override) a virtual function
+		-Wpedantic                   # Warn if non-standard C++ is used
+		-Wconversion                 # Warn on type conversions that may lose data
+		-Wsign-conversion            # Warn on sign conversions
+		-Wnull-dereference           # Warn if a null dereference is detected
+		-Wdouble-promotion           # Warn if float is implicit promoted to double
+		-Wformat=2                   # Warn on security issues around functions that format output (ie printf)
+
+		$<$<CONFIG:Release,RelWithDebInfo,MinSizeRel>:-Werror> # Warnings as errors
+	)
 
 	set(GCC_WARNINGS
-			${CLANG_WARNINGS}
-			-Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
-			-Wduplicated-cond # warn if if / else chain has duplicated conditions
-			-Wduplicated-branches # warn if if / else branches have duplicated code
-			-Wlogical-op # warn about logical operations being used where bitwise were probably wanted
-			-Wuseless-cast # warn if you perform a cast to the same type
-			)
+		${CLANG_WARNINGS}
+		-Wmisleading-indentation # Warn if indentation implies blocks where blocks do not exist
+		-Wduplicated-cond        # Warn if if / else chain has duplicated conditions
+		-Wduplicated-branches    # Warn if if / else branches have duplicated code
+		-Wlogical-op             # Warn about logical operations being used where bitwise were probably wanted
+		-Wuseless-cast           # Warn if you perform a cast to the same type
+	)
 
 	set(targetWarnings)
 	if(MSVC)
